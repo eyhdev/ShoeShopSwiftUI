544b2de5623da531ecd1fe9269e254870eeccbd8
eyhdev <alaqsa335@gmail.com>
2023-09-17 15:41:49 +0300

    Initial Commit

---
+++ ShoeShop/ShoeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeShopApp.swift
+//  ShoeShop
+//
+//  Created by eyh.mac on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
